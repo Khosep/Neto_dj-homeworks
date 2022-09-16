6478ed10c985ba27770ec3dd4413bb14e2434acf
Khosep <khose007@yandex.ru>
2022-09-16 22:15:53 +0500

    hw-2.2_m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        number_of_main_tags = 0
+        tags = []
+        for form in self.forms:
+            if form.cleaned_data.get('is_main') and form.cleaned_data:
+                number_of_main_tags += 1
+            tag = form.cleaned_data.get('tag')
+            if tag in tags:
+                raise ValidationError(f'Раздел {tag} уже добавлен')
+            tags.append(tag)
+        # number_of_main_tags = sum([form.cleaned_data.get('is_main') for form in self.forms if form.cleaned_data])
+        if not number_of_main_tags:
+            raise ValidationError('Укажите основной раздел')
+        elif number_of_main_tags > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'published_at']
+    list_filter = ['published_at']
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
@@ -11,6 +10,28 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True, verbose_name='Название раздела')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['name']
+
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ['-is_main', 'tag']
